bed8dca1355f01d5a753c15b01a9b445244818b1
Rudrasharma2206 <rudra.sharma6667@gmail.com>
2026-07-24 16:13:46 +0530

    Time: 6 ms (87.98%), Space: 43.8 MB (93.25%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,32 +1,30 @@
 class Solution {
         public static boolean checkInclusion(String s1, String s2) {
-        if(s1.length()>s2.length()) return false;
-        HashMap<Character,Integer> map1=new HashMap<>();
-        HashMap<Character,Integer> map2=new HashMap<>();
-        int n=s1.length();
-        for(int i=0;i<n;i++){
-            Character x=s1.charAt(i);
-            map1.put(x,map1.getOrDefault(x,0)+1);
-        }
-        int j=0;
-        for(int i=0;i<s2.length();i++){
-            Character y=s2.charAt(i);
-            map2.put(y,map2.getOrDefault(y,0)+1);
-            if(i-j+1==n){
-                if(map1.equals(map2)){
-                    return true;
-                }
-                Character leftChar = s2.charAt(j);
+    if (s1.length() > s2.length()) return false;
 
+    int[] map1 = new int[26];
+    int[] map2 = new int[26];
+    int n = s1.length();
 
-                map2.put(leftChar, map2.get(leftChar) - 1);
-
-                if (map2.get(leftChar) == 0) {
-                    map2.remove(leftChar);
-                }
-                j++;
-            }
-        }
-        return false;
+    // Populate frequency for s1 and the first window of s2
+    for (int i = 0; i < n; i++) {
+        map1[s1.charAt(i) - 'a']++;
+        map2[s2.charAt(i) - 'a']++;
     }
+
+    // Slide the window across s2
+    for (int i = n; i < s2.length(); i++) {
+        // 1. Check if current windows match
+        if (Arrays.equals(map1, map2)) return true;
+
+        // 2. Add new character entering from right
+        map2[s2.charAt(i) - 'a']++;
+
+        // 3. Remove character leaving from left
+        map2[s2.charAt(i - n) - 'a']--;
+    }
+
+    // Check final window state
+    return Arrays.equals(map1, map2);
+}
 }
